61d4f88eb4dd1952308b8e6666990ba889b864a1
minhaz11 <minhaz217@gmail.com>
2020-04-11 08:57:18 -0600

    looping

---
+++ 10.Looping/app.js
@@ -0,0 +1,11 @@
+new Vue ({
+    el:'#app',
+    data:{
+        characters:['Rio','Nathan','John','Smith'],
+        ninjas:[
+          {name:'Rio',age:25},
+          {name:'Nathan',age:29},
+          {name:'Yushi',age:35},
+        ]
+    }
+})
